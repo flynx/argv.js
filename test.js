167cbf12583f63424105080bbb27c31225808280
Alex A. Naanou <alex.nanou@gmail.com>
2020-06-14 03:10:19 +0300

    migrated code from ig-object/test.js...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

---
+++ test.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+/**********************************************************************
+* 
+*
+*
+**********************************************************************/
+((typeof define)[0]=='u'?function(f){module.exports=f(require)}:define)
+(function(require){ var module={} // make module AMD/node compatible...
+/*********************************************************************/
+
+var colors = require('colors')
+var object = require('ig-object')
+
+var argv = require('./argv')
+
+
+//---------------------------------------------------------------------
+
+
+
+
+
+/**********************************************************************
+* vim:set ts=4 sw=4 :                               */ return module })
